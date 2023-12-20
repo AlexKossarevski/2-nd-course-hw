b4291ccd8d8ef3af23c195055ae52244f993fd65
AlexKossarevski <work.kosarevski@gmail.com>
2023-12-20 19:22:15 +0600

    8 задание

--- hw-2.2.js
+++ hw-2.2.js
@@ -1,8 +1,44 @@
 //Задание 1
-//let a = 10;
-//alert(a)
-
-//let a = 20;
-//alert(a)
+//  let a = 10;
+//  alert(a)
+//  let a = 20;
+//  alert(a)
 
 //Задание 2
+//  let startiPhone = 2007 + " " + "год.";
+//  alert(startiPhone)
+
+//Задание 3
+//  let creatorJS = 'Brendan Eich';
+//  alert(creatorJS)
+
+//Задание 4
+//  let variable_1 = 10;
+//  let variable_2 = 2;
+//  alert(variable_1 + variable_2)
+//  alert(variable_1 - variable_2)
+//  alert(variable_1 * variable_2)
+//  alert(variable_1 / variable_2)
+
+//Задание 5
+//  let result = 2 ** 5;
+//  alert(result)
+
+//Задание 6
+//  let a = 9;
+//  let b = 2;
+//  alert(a/b)
+
+//Задание 7
+//let num = 1;
+//num += 5;          //num = num + 5;
+//num -= 3;          //  num = num - 3;
+//num *= 7;          //num = num * 7;
+//num /= 3;          //num = num / 3;
+//num += 1;         //num = num + 1;
+//num -= 1;         //num = num - 1;
+//alert(num);
+
+//Задание 8
+//let age = "Сколько вам лет?";
+//prompt(age)
