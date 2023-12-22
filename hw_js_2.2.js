452f1d7bc06146b2411035e57d887b18718b661d
AlexKossarevski <work.kosarevski@gmail.com>
2023-12-22 13:48:36 +0600

    Домашка 2

---
+++ hw_js_2.2.js
@@ -0,0 +1,61 @@
+//Задание 1
+//  let a = 10;
+//  alert(a)
+//  a = 20;
+//  alert(a)
+
+//Задание 2
+//  let startiPhone = 2007 + " " + "год.";
+//  alert(startiPhone)
+
+//Задание 3
+//  let creatorJS = 'Brendan Eich';
+//  alert(creatorJS)
+
+//Задание 4
+//  let variable_1 = 10;
+//  let variable_2 = 2;
+//  alert(variable_1 + variable_2)
+//  alert(variable_1 - variable_2)
+//  alert(variable_1 * variable_2)
+//  alert(variable_1 / variable_2)
+
+//Задание 5
+//  let result = 2 ** 5;
+//  alert(result)
+
+//Задание 6
+//  let a = 9;
+//  let b = 2;
+//  alert(a/b)
+
+//Задание 7
+//let num = 1;
+//num += 5;          //num = num + 5;
+//num -= 3;          //  num = num - 3;
+//num *= 7;          //num = num * 7;
+//num /= 3;          //num = num / 3;
+//num += 1;         //num = num + 1;
+//num -= 1;         //num = num - 1;
+//alert(num);
+
+//Задание 8
+//let age = prompt('Сколько вам лет?');
+
+//Задание 9   9.1   9.2  9.3   9.4
+//  const user = {
+//  name:'Alex',
+//  age:40,
+//  isAdmin:true,
+//  }
+//  user["city of residence"] = 'Astana',
+//  user.age = 32,
+//  delete user["city of residence"],
+//  console.log (user);
+//  let info = prompt('Какую информацию хотите узнать о пользователе?');
+//  alert(user[info]);
+
+//Задание 10
+//  let yName = prompt('Как тебя зовут?');
+//  let result = yName;
+//  alert(`Привет, ${result}!`);
