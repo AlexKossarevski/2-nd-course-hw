196f457013e0c759e34874e9fc103a6819829e3b
AlexKossarevski <work.kosarevski@gmail.com>
2023-12-27 21:55:25 +0600

    js

--- hw_2.3.js
+++ hw_2.3.js
@@ -27,45 +27,45 @@
 //  let b = '3';
 //  alert(Number (a) + Number (b));
 //   ****************************************** Задание 5 ****************************
-let monthNumber = prompt ('Введите номер месяца.');
-switch (monthNumber) {
-    case '1':
-        console.log('Январь. Зима');
-        break;
-    case '2':
-        console.log('Февраль. Зима');
-        break;
-    case '3':
-        console.log('Март. Весна');
-        break;
-    case '4':
-        console.log('Апрель. Весна');
-        break;
-    case '5':
-        console.log('Май. Весна');
-        break;
-    case '6':
-        console.log('Июнь. Лето');
-        break;
-    case '7':
-        console.log('Июль. Лето');
-        break;
-    case '8':
-        console.log('Август. Лето');
-        break;
-    case '9':
-        console.log('Сентябрь. Осень');
-        break;
-    case '10':
-        console.log('Октябрь. Осень');
-        break;
-    case '11':
-        console.log('Ноябрь. Осень');
-        break;
-    case '12':
-        console.log('Декабрь. Зима');
-        break;
-    default:
-        console.log('Такого месяца нет, ты из какой вселенной!?');
-        break;
-}
+// let monthNumber = prompt ('Введите номер месяца.');
+// switch (monthNumber) {
+//     case '1':
+//         console.log('Январь. Зима');
+//         break;
+//     case '2':
+//         console.log('Февраль. Зима');
+//         break;
+//     case '3':
+//         console.log('Март. Весна');
+//         break;
+//     case '4':
+//         console.log('Апрель. Весна');
+//         break;
+//     case '5':
+//         console.log('Май. Весна');
+//         break;
+//     case '6':
+//         console.log('Июнь. Лето');
+//         break;
+//     case '7':
+//         console.log('Июль. Лето');
+//         break;
+//     case '8':
+//         console.log('Август. Лето');
+//         break;
+//     case '9':
+//         console.log('Сентябрь. Осень');
+//         break;
+//     case '10':
+//         console.log('Октябрь. Осень');
+//         break;
+//     case '11':
+//         console.log('Ноябрь. Осень');
+//         break;
+//     case '12':
+//         console.log('Декабрь. Зима');
+//         break;
+//     default:
+//         console.log('Такого месяца нет, ты из какой вселенной!?');
+//         break;
+// }
