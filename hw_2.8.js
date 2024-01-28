89a728e56350ec9688edcaf83f8e1b74dcd05183
AlexKossarevski <work.kosarevski@gmail.com>
2024-01-29 02:33:08 +0600

    2.8 домашка

--- hw_2.8.js
+++ hw_2.8.js
@@ -1,68 +1,105 @@
 //=============================================== 1 Task =============================================
-const people = [
-    { name: 'Глеб', age: 29 },
-    { name: 'Анна', age: 17 },
-    { name: 'Олег', age: 7 },
-    { name: 'Оксана', age: 47 }
-];
+// const people = [
+//     { name: 'Глеб', age: 29 },
+//     { name: 'Анна', age: 17 },
+//     { name: 'Олег', age: 7 },
+//     { name: 'Оксана', age: 47 }
+// ];
 
 
-console.log(people.sort(function sortAge(a, b) {
-    if (a.age > b.age) {
-        return 1;
-    }
-    if (a.age < b.age) {
-        return -1;
-    }
-    return 0;
-}));
+// console.log(people.sort(function sortAge(a, b) {
+//     if (a.age > b.age) {
+//         return 1;
+//     }
+//     if (a.age < b.age) {
+//         return -1;
+//     }
+//     return 0;
+// }));
 
 
 
 
 // console.log(people.sort((a, b) => a.age - b.age));
 //=============================================== 2 Task =============================================
-
-// function isPositive(number) {
-//     return number > 0;
+// function isPositive(num) {
+//     return num > 0;
 // }
 
-// function filter(arr, ruleFunction) {
-//     const output = [];
-//     for (let i = 0; i < arr.length; i++) {
-//         if (ruleFunction(arr[i])) {
-//             output.push(arr[i]);
+// console.log(filter([3, -4, 1, 9], isPositive));
+
+
+// function isMale(person) {
+//     return person.gender === 'male';
+// }
+
+// function filter(array, ruleFunction) {
+//     let newArray = [];
+//     for (let i = 0; i < array.length; i++) {
+//         if (ruleFunction(array[i])) {
+//             newArray.push(array[i]);
 //         }
 //     }
-//     return output;
+//     return newArray;
 // }
-// console.log(filter([3, -4, 1, 9], isPositive)); // Должен выводить [3, 1, 9]
+
+// const people = [
+//     { name: 'Глеб', gender: 'male' },
+//     { name: 'Анна', gender: 'female' },
+//     { name: 'Олег', gender: 'male' },
+//     { name: 'Оксана', gender: 'female' }
+// ];
+
+// console.log(filter(people, isMale)); // Должен выводить [{name: 'Глеб', gender: 'male'},  {name: 'Олег', gender: 'male'}]
 
 //=============================================== 3 Task =============================================
-// const date = () => {
-//     let currentDate = new Date();
-//     let interval = setInterval(() => console.log(currentDate), 3000);
-//     setTimeout(() => {
-//         clearInterval(interval);
-//         console.log(date('30 секунд прошло!'));
+// let interval;
+// let counter = 0;
 
-//     }, 30000);
-// }
-
-// const date = () => {
-//     let currentDate = new Date();
-//     let interval = setInterval(() => console.log(currentDate), 3000);
+// function logCurrentDate() {
+//     const currentDate = new Date();
 //     console.log(currentDate);
+//     counter += 3;
+//     if (counter >= 30) {
+//         clearInterval(interval);
+//         console.log('30 секунд прошло');
+//     }
 // }
 
-// let currentDate = new Date();
-// let interval = setInterval(() => console.log(currentDate), 3000);
+// interval = setInterval(logCurrentDate, 3000);
 
 //=============================================== 4 Task =============================================
+// function delayForSecond(callback) {  // Код писать можно только внутри этой функции  // setTimeout
+//     setTimeout(callback, 1000);
+// }
 
-
+// delayForSecond(function () {
+//     console.log('Привет, Глеб!');
+// })
 //=============================================== 5 Task =============================================
+// Функция delayForSecond через 1 секунду пишет в консоль «Прошла одна секунда», 
+// а затем вызывает переданный колбэк
+// function delayForSecond(cb) {
+//     setTimeout(() => {
+//         console.log('Прошла одна секунда');
+//         if (cb) { cb(); }
 
+//     }, 1000)
+// }
+
+// // Функция sayHi выводит в консоль приветствие для указанного имени
+// function sayHi(name) {
+//     console.log(`Привет, ${name}!`);
+// }
+
+// // Код выше менять нельзя
+
+// // Нужно изменить код ниже:
+// // delayForSecond(sayHi('Глеб'))
+
+// delayForSecond(() => {
+//     sayHi('Глеб');
+// });
 
 //--------------------------------------------------------------------------------//
 //----------------------------------------------------------------------------------//
